a607f6c9a9d19ea16eb90e22fd307cd4c0c52cd0
Alan <81753503+popalanluck@users.noreply.github.com>
2024-03-14 16:36:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,7 +5,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages -b main feeds/p
 git clone https://github.com/tty228/luci-app-wechatpush -b master feeds/packages/custom/luci-app-wechatpush
 git clone https://github.com/hubbylei/openssl feeds/packagescustom/openssl
 git clone https://github.com/hubbylei/wrtbwmon -b master feeds/packages/custom/wrtbwmon
-git clone https://github.com/coolsnowwolf/packages -b master feeds/packages/tmp/packages
+git clone https://github.com/coolsnowwolf/packages -b master tmp/packages
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 git clone https://github.com/Ausaci/luci-app-nat6-helper feeds/packages/luci-app-nat6-helper
