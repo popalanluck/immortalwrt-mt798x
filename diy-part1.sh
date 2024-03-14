d295174ed2bcf7c47753d079f2ca112c3c8727b4
Alan <81753503+popalanluck@users.noreply.github.com>
2024-03-14 16:28:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,11 @@
+git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/custom/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns -b master feeds/packages/custom/luci-app-smartdns
+git clone https://github.com/xiaorouji/openwrt-passwall -b main feeds/packagescustom/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages -b main feeds/packages/custom/openwrt-passwall
+git clone https://github.com/tty228/luci-app-wechatpush -b master feeds/packages/custom/luci-app-wechatpush
+git clone https://github.com/hubbylei/openssl feeds/packagescustom/openssl
+git clone https://github.com/hubbylei/wrtbwmon -b master feeds/packages/custom/wrtbwmon
+git clone https://github.com/coolsnowwolf/packages -b master feeds/packages/tmp/packages
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 git clone https://github.com/Ausaci/luci-app-nat6-helper feeds/packages/luci-app-nat6-helper
